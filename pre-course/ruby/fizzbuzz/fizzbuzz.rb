f4e2a1b7d8601606c57c773c466fa2c2bf3456f7
PatrickHourihan <YOUR EMAIL ADDRESShouripj357@gmail.com>
2016-04-30 21:27:19 -0400

    second iteration using upto

--- pre-course/ruby/fizzbuzz/fizzbuzz.rb
+++ pre-course/ruby/fizzbuzz/fizzbuzz.rb
@@ -1,11 +1,23 @@
-(1..100).each do |i|
-	if i % 3 == 0 && i % 5 == 0
-		puts "FizzBuzz"
+# (1..100).each do |i|
+# 	if i % 3 == 0 && i % 5 == 0
+# 		puts "FizzBuzz"
+# 	elsif i % 3 == 0
+# 		puts "Fizz"
+# 	elsif i % 5 == 0
+# 		puts "Buzz"
+# 	else 
+# 		puts i
+# end
+# end
+
+1.upto 100 do |i|
+	if i % 3 == 0 && i % 5 ==0 
+		puts 'FizzBuzz'
 	elsif i % 3 == 0
-		puts "Fizz"
+		puts 'Fizz'
 	elsif i % 5 == 0
-		puts "Buzz"
-	else 
-		puts i
+		puts 'Buzz'
+	else puts i
 end
 end
+

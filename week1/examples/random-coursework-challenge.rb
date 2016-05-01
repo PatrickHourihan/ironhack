c0e202b71521e319df50c10ce885cc1a9eefb364
PatrickHourihan <YOUR EMAIL ADDRESShouripj357@gmail.com>
2016-04-30 21:12:32 -0400

    add multiple method examples

--- week1/examples/random-coursework-challenge.rb
+++ week1/examples/random-coursework-challenge.rb
@@ -6,12 +6,29 @@
 
 cities = ["miami", "madrid", "barcelona"]
 
- cap_cities = cities.map do | city |
+ # cap_cities = cities.map do | city |
 	
- 	city.capitalize
- end
+ # 	city.capitalize
+ # end
 
- cap_cities = cities.map { | city | city.capitalize }
+ # cap_cities = cities.map { | city | city.capitalize }
 
- p cap_cities
+ # p cap_cities
 
+ # Reduce 
+
+ # total = [1, 2, 3].reduce(10) do | sum, num |
+ # 	sum + num
+ # end
+
+ # puts total 
+
+ # total_length = cities.reduce(0) do | sum, city_length |
+ # 	sum + city_length.length 
+ # end 
+
+ # puts total_length / cities.length
+
+cities.each_with_index do | city, index |
+  puts "#{index + 1}: #{city}"
+end

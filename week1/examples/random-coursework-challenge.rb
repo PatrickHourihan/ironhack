17856518c7be4d2b6fa645784cc920fcdefe9bb0
PatrickHourihan <YOUR EMAIL ADDRESShouripj357@gmail.com>
2016-04-30 21:04:51 -0400

    map method on new cities array for example of capitalize method

--- week1/examples/random-coursework-challenge.rb
+++ week1/examples/random-coursework-challenge.rb
@@ -1,5 +1,17 @@
- classmates = ["Ryan", "Dieter", "Melike"]
+ # classmates = ["Ryan", "Dieter", "Melike"]
 
- classmates.each do | name |
- 	puts "Good morning #{name} :)"
+ # classmates.each do | name |
+ # 	puts "Good morning #{name} :)"
+ # end
+
+cities = ["miami", "madrid", "barcelona"]
+
+ cap_cities = cities.map do | city |
+	
+ 	city.capitalize
  end
+
+ cap_cities = cities.map { | city | city.capitalize }
+
+ p cap_cities
+

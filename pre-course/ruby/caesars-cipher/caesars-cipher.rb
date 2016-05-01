db6c4f9f84d754d3171720bbd913a1d7490cb772
PatrickHourihan <YOUR EMAIL ADDRESShouripj357@gmail.com>
2016-04-30 21:22:34 -0400

    commit to add code from class to github

---
+++ pre-course/ruby/caesars-cipher/caesars-cipher.rb
@@ -0,0 +1,30 @@
+def solve_cipher(input)
+  words = input.split("")
+
+string_container = ""
+
+  words.each do | letter |
+  	string_container += (letter.ord + -1).chr
+  end
+  string_container
+end
+
+puts solve_cipher("ifmmp")
+# should return "hello"
+
+def solve_cipher(input, shift)
+  words = input.split("")
+
+string_container = ""
+  words.each do | letter |
+	if letter == " "
+		string_container += " "
+	else
+  	string_container += (letter.ord + shift).chr
+  end
+  end
+  string_container
+end
+
+puts solve_cipher("p| uhdo qdph lv grqdog gxfn", -3)
+# should return "my real name is donald duck"

c6c15c1d0d2ceed0945896eaba35d542435dec93
PatrickHourihan <YOUR EMAIL ADDRESShouripj357@gmail.com>
2016-05-14 23:48:07 -0400

    first attempt gameplay method

--- week2/homework/app.rb
+++ week2/homework/app.rb
@@ -4,19 +4,19 @@ require_relative('lib/Player')
 
 
 my_board = Board.new
-# my_board.show_board
-# my_board.add_character([0,0], "X")
-# my_board.show_board
-# my_board.add_character([1,1], "X")
-# my_board.show_board
-# my_board.add_character([1,1], "0")
-# my_board.show_board
-# my_board.add_character([2,2], "O")
-# my_board.show_board
-# my_board.add_character([0,2], "X")
-# my_board.show_board
-# my_board.add_character([2,0], "X")
-# my_board.show_board
+my_board.show_board
+my_board.add_character([0,0], "X")
+my_board.show_board
+my_board.add_character([1,1], "X")
+my_board.show_board
+my_board.add_character([1,1], "0")
+my_board.show_board
+my_board.add_character([2,2], "O")
+my_board.show_board
+my_board.add_character([0,2], "X")
+my_board.show_board
+my_board.add_character([2,0], "X")
+my_board.show_board
 
 
 # my_board.numbered_board

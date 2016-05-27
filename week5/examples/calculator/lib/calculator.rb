0317abba28ca0f48cb086505f825aa20480e4b9e
PatrickHourihan <YOUR EMAIL ADDRESShouripj357@gmail.com>
2016-05-27 00:00:04 -0400

    add cool new ruby calculator

---
+++ week5/examples/calculator/lib/calculator.rb
@@ -0,0 +1,40 @@
+class Calculator
+
+	def add
+		puts "enter a number"
+		num1 = (gets.chomp).to_f
+		puts "enter a number"
+		num2 = (gets.chomp).to_f
+		puts (num1 + num2)
+	end
+	
+	def subtract
+		puts "enter a number"
+		num1 = (gets.chomp).to_f
+		puts "enter a number"
+		num2 = (gets.chomp).to_f
+		puts num1 - num2
+	end
+
+	def multiply
+		puts "enter a number"
+		num1 = (gets.chomp).to_f
+		puts "enter a number"
+		num2 = (gets.chomp).to_f
+		puts num1 * num2
+	end
+
+	def divide
+		puts "enter a number"
+		num1 = (gets.chomp).to_f
+		puts "enter a number"
+		num2 = (gets.chomp).to_f
+		puts num1 / num2
+	end
+end
+
+test_calculator = Calculator.new
+test_calculator.add
+test_calculator.subtract
+test_calculator.multiply
+test_calculator.divide

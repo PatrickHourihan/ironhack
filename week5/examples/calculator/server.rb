a6f5a40551560658340ffc62f943939178f41cf8
PatrickHourihan <YOUR EMAIL ADDRESShouripj357@gmail.com>
2016-05-26 23:47:49 -0400

    iterations 1-3 completed, which included creating several forms at first and condensing into 1

---
+++ week5/examples/calculator/server.rb
@@ -0,0 +1,79 @@
+# server.rb
+
+require "sinatra"
+
+get "/" do
+  "You can <a href='http://localhost:4567/operate'>OPERATE</a>, or: <a href='http://localhost:4567/add'>add</a>, <a href='http://localhost:4567/subtract'>subtract</a>, <a href='http://localhost:4567/multiply'>multiply</a> and <a href='http://localhost:4567/divide'>divide</a>"
+end
+
+get "/operate" do 
+  erb(:operate)
+end
+
+post "/calculate_operate" do
+  first = params[:first_number].to_f
+  second = params[:second_number].to_f
+
+  operation = params[:operation]
+
+    if operation == "add" 
+	  result = first + second
+      "#{first} + #{second} = #{result}"
+
+    elsif operation == "subtract" 
+	  result = first - second
+	  "#{first} - #{second} = #{result}"
+
+	elsif operation == "multiply" 
+  	  result = first * second
+  	  "#{first} * #{second} = #{result}"
+
+	elsif operation == "divide" 
+  	  result = first / second
+  	  "#{first} / #{second} = #{result}"
+  	end
+end
+
+get "/add" do
+  erb(:add)
+end
+
+post "/calculate_add" do
+  first = params[:first_number].to_f
+  second = params[:second_number].to_f
+  result = first + second
+  "#{first} + #{second} = #{result}"
+end
+
+get "/subtract" do 
+  erb(:subtract)
+end
+
+post "/calculate_subtract" do 
+  first = params[:first_number].to_f
+  second = params[:second_number].to_f
+  result = first - second
+  "#{first} - #{second} = #{result}"
+end
+
+get "/multiply" do 
+  erb(:multiply)
+end
+
+post "/calculate_multiply" do 
+  first = params[:first_number].to_f
+  second = params[:second_number].to_f
+  result = first * second
+  "#{first} * #{second} = #{result}"
+end
+
+get "/divide" do 
+  erb(:divide)
+end
+
+post "/calculate_divide" do 
+  first = params[:first_number].to_f
+  second = params[:second_number].to_f
+  result = first / second
+  "#{first} / #{second} = #{result}"
+end

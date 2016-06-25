456a98982571090581d9806ac27afba6c6d77142
houripj357 <houripj357@gmail.com>
2016-06-25 14:36:44 -0400

    modify link to paths

--- week9/exercises/ironbnb/config/routes.rb
+++ week9/exercises/ironbnb/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
-  get '/' => 'site#home'
+  # get '/' => 'site#home'
+  
+  root 'site#home'
 
   get '/become_a_host' => 'site#becomeAHost'
+
+  get '/wish_lists' => 'site#wishLists'
 
   get '/help' => 'site#help'
 
